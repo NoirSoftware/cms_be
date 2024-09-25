2ed70b626b5b0f341ca3a027622064963f2f7091
NoirSoftware <noir.software.bt@gmail.com>
2024-09-25 19:02:37 +0200

    db connection base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',  // or your PostgreSQL host
+    port: 5432,
+    username: 'cms',  // your PostgreSQL username
+    password: 'mypassword',  // your PostgreSQL password
+    database: 'cms',     // your PostgreSQL database name
+    autoLoadEntities: true,    // automatically load entities (set to false if you manually manage entities)
+    synchronize: true,         // set to true only in development, as it auto-syncs the schema
+  }), UsersModule
+]
 })
 export class AppModule {}
